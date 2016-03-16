247d3cf82aa80d0bd695991680b8799f303ef913
Gustavo Romero <gromero@br.ibm.com>
2016-03-16 14:40:21 -0300

    Add loop to try avoid noise on perf profiling

--- arraycopy.c
+++ arraycopy.c
@@ -5,6 +5,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <strings.h>
 
 // 16 MiB, or 1 M 64-bit elements
 #define CHUNK_SIZE 16*1024*1024
@@ -21,14 +22,15 @@ void arraycopy(uint64_t *dst, uint64_t *src, size_t n)
   i = n / 4;
 
   printf("Copying %ld 64-bit element(s), "  \
-             "with %ld block iteration(s) " \
+            "with %ld block iteration(s) " \
              "and %ld byte(s) as remainder(s)\n", n, i, remainder);
 
   asm (
-       "1:      cmpldi %2, 0    \n\t"
-       "        beq- 1f         \n\t"
+       "        cmpldi %2, 0    \n\t"
+       "        beq- 2f         \n\t"
+       "        mtctr %2	\n\t"
        /********* Main Code ********/
-       "        ld  3, 0(%1)    \n\t"
+       "1:      ld  3, 0(%1)    \n\t"
        "        ld  4, 8(%1)    \n\t"
        "        ld  5,16(%1)    \n\t"
        "        ld  6,24(%1)    \n\t"
@@ -39,13 +41,11 @@ void arraycopy(uint64_t *dst, uint64_t *src, size_t n)
        /****************************/
        "        addi %1, %1, 32 \n\t"
        "        addi %0, %0, 32 \n\t"
-       "        subi %2, %2, 1  \n\t"
-       "        cmpldi %2, 0    \n\t"
-       "        bgt+ 1b         \n\t"
-       "1:      nop             \n\t"
+       "        bdnz 1b		\n\t"
+       "2:      nop             \n\t"
         :
         : "r"(dst), "r"(src), "r"(i)
-        : "memory", "3", "4", "5", "6"
+        : "memory", "r3", "r4", "r5", "r6"
        );
 
   for (int j = i*4; j < n; ++j)
@@ -68,25 +68,26 @@ void arraycopy(uint64_t *dst, uint64_t *src, size_t n)
                  "and %ld byte(s) as remainder(s)\n", n, i, remainder);
 
   asm (
-       "1:      cmpldi %2, 0    \n\t"
-       "        beq- 1f         \n\t"
+       "1:      cmpldi %2, 0     \n\t"
+       "        beq- 1f          \n\t"
+       "        li 5, 16	 \n\t"
+       "        mtctr  %2	 \n\t"
        /********* Main Code ********/
-       "2:      lxvx  3, 0, %1  \n\t"
-       "        addi %1, %1, 16 \n\t"
-       "        lxvx  4, 0, %1  \n\t"
-       "        addi %1, %1, 16 \n\t"
-       "        stxvx 3, 0, %0  \n\t"
-       "        addi %0, %0, 16 \n\t"
-       "        stxvx 4, 0, %0  \n\t"
-       "        addi %0, %0, 16 \n\t"
+       "2:      lxvd2x  3, 0, %1 \n\t"
+       "        lxvd2x  4, %1, 5 \n\t"
+       "        addi %1, %1, 16  \n\t"
+       "        stxvd2x 3, 0, %0 \n\t"
+       "        stxvd2x 4, %0, 5 \n\t"
+       "      	addi %0, %0, 16  \n\t"
        /****************************/
-       "        subi %2, %2, 1  \n\t"
-       "        cmpldi %2, 0    \n\t"
-       "        bgt+ 2b         \n\t"
-       "1:      nop             \n\t"
+       "	bdnz  2b	 \n\t"
+//     "        subi %2, %2, 1   \n\t"
+//     "        cmpldi %2, 0     \n\t"
+//     "        bgt+ 2b          \n\t"
+       "1:      nop              \n\t"
         :
         : "r"(dst), "r"(src), "r"(i)
-        : "memory", "3", "4"
+        : "memory", "3", "4", "5"
        );
 
   for (int j = i*4; j < n; ++j)
@@ -98,8 +99,14 @@ int main(void)
 {
   printf("** Inline ASM for VSX test **\n\n");
 
-  uint64_t* source      = malloc(CHUNK_SIZE); // 16 MiB, or 1 MiB 64-bit element(s)
-  uint64_t* destination = malloc(CHUNK_SIZE); // 16 MiB, or 1 MiB 64-bit element(s)
+  uint64_t* source      = malloc(CHUNK_SIZE); // 16 MiB, or 1 MiB 64-bit element(s).
+  uint64_t* destination = malloc(CHUNK_SIZE); // 16 MiB, or 1 MiB 64-bit element(s).
+  uint64_t* zero	= malloc(CHUNK_SIZE); // To hold zeros.
+
+  uint64_t* aux1;
+  uint64_t* aux2;
+
+  bzero(zero, CHUNK_SIZE);
 
   int fd, rc;
 
@@ -120,8 +127,13 @@ int main(void)
 
   printf("2. Exercising...\n");
 
-  // Copy 16 MiB, or 1 M 64-bit (8 bytes) elements.
-  arraycopy(destination, source, 1024*1024);
+  // Spend some time here.
+  for (int p; p < 2500; ++p) {
+    // Copy 16 MiB, or 1 M 64-bit (8 bytes) elements.
+    arraycopy(destination, source, 1024*1024);
+    // Just zero destination.
+    arraycopy(destination, zero,   1024*1024);
+  }
 
   printf("2. Done.\n");
 
