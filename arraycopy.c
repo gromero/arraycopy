ea1e32eebe4b106c5d951df30df8bcfc5aa8b901
Gustavo Romero <gromero@br.ibm.com>
2016-03-16 18:58:47 -0300

    wip

--- arraycopy.c
+++ arraycopy.c
@@ -7,19 +7,23 @@
 #include <unistd.h>
 #include <strings.h>
 
-// Bytes per element. In this example
-// elements are 64-bit or 8 bytes.
-#define ELEM_SIZE 8
+// Number of elements inside buffer and to be
+// loaded and stored between two different
+// locations in the storage.
+#define NUM_OF_ELEM_IN_BUFFER 1 <-----------
+
+// Number of bytes occupied by an element in buffer. Since
+// in this particular example we are using uint64_t, an element
+// is a 64-bit entity and, as such, its size is 8 bytes.
+#define ELEMENT_SIZE 8
 
 // 32 bytes (or 4 64-bit elements) copied
-// at a time (per loop).
+// at a time (per loop - see inline ASM).
 #define BULK_COPY_SIZE 32
 
-// General scratch buffer size is 32 MiB,
-// or 1024 x 1024 x (4 64-bit elements ==
-// 8 bytes).
-#define SCRATCH_BUFFER_SIZE ELEM_SIZE
-
+// Calculate buffer size in bytes able to hold a given
+// number of elements.
+#define BUFFER_SIZE NUM_OF_ELEM_IN_BUFFER * ELEMENT_SIZE
 
 
 #if !defined(VSX)
@@ -28,21 +32,21 @@ void arraycopy(uint64_t *dst, uint64_t *src, size_t n /* # of 64-bit elements */
   size_t loop;
   size_t remainder;
 
-  loop      = n / (BULK_COPY_SIZE / ELEM_SIZE); // n / 4 64-bit elements.
-  remainder = n % (BULK_COPY_SIZE / ELEM_SIZE); // n % 4 64-bit elements.
+  loop      = n / (BULK_COPY_SIZE / ELEMENT_SIZE); // n / 4, Four 64-bit elements.
+  remainder = n % (BULK_COPY_SIZE / ELEMENT_SIZE); // n % 4, Four 64-bit elements.
 
 #if defined(DEBUG)
   printf("Copying %ld 64-bit element(s), " \
             "with %ld block iteration(s) " \
-             "and %ld byte(s) as remainder(s)\n", n, i, remainder);
+             "and %ld byte(s) as remainder(s)\n", n, loop, remainder);
 #endif
 
   asm (
-       "        cmpldi %2, 0    \n\t"
-       "        beq- 2f         \n\t"
-       "        mtctr %2	\n\t"
+       "        cmpldi %2, 0    \n\t" // Check if n < 4
+       "        beq- 2f         \n\t" //
+       "        mtctr %2        \n\t" // If not, set counter
        /********* Main Code ********/
-       "1:      ld  3, 0(%1)    \n\t"
+       "1:      ld  3, 0(%1)    \n\t" // Copy 4 elements.
        "        ld  4, 8(%1)    \n\t"
        "        ld  5,16(%1)    \n\t"
        "        ld  6,24(%1)    \n\t"
@@ -50,55 +54,57 @@ void arraycopy(uint64_t *dst, uint64_t *src, size_t n /* # of 64-bit elements */
        "        std 4, 8(%0)    \n\t"
        "        std 5,16(%0)    \n\t"
        "        std 6,24(%0)    \n\t"
+       "        addi %1, %1, 32 \n\t" // Update src pointer.
+       "        addi %0, %0, 32 \n\t" // Update dst pointer.
        /****************************/
-       "        addi %1, %1, 32 \n\t"
-       "        addi %0, %0, 32 \n\t"
-       "        bdnz 1b		\n\t"
-       "2:      nop             \n\t"
+       "        bdnz 1b         \n\t" // Dec. counter, if zero
+       "2:      nop             \n\t" // jump here (otherwise loop 1).
         :
         : "r"(dst), "r"(src), "r"(loop)
         : "memory", "r3", "r4", "r5", "r6"
        );
 
-  for (int j = i*4; j < n; ++j)
+  // Deal with remainers and if n < 4.
+  for (int j = loop * 4; j < n; ++j)
     dst[j] = src[j];
 }
 
 #else
-void arraycopy(uint64_t *dst, uint64_t *src, size_t n)
+void arraycopy(uint64_t *dst, uint64_t *src, size_t n /* # of 64-bit elements */)
 {
-  size_t i = 0;
-  size_t remainder = 0;
+  size_t loop;
+  size_t remainder;
 
-  // Bulk rd/wr size is 4, ie 4 x 8 bytes, or
-  // 4 x 64-bit elements rd/wr "at once".
-  remainder = n % 4;
-  i = n / 4;
+  loop      = n / (BULK_COPY_SIZE / ELEMENT_SIZE); // n / 4, Four 64-bit elements.
+  remainder = n % (BULK_COPY_SIZE / ELEMENT_SIZE); // n % 4, Four 64-bit elements.
+
 #if defined(DEBUG)
-  printf("VSX copying %ld 64-bit element(s), " \
-                "with %ld block iteration(s) " \
-                 "and %ld byte(s) as remainder(s)\n", n, i, remainder);
+  printf("Copying %ld 64-bit element(s), " \
+            "with %ld block iteration(s) " \
+             "and %ld byte(s) as remainder(s)\n", n, loop, remainder);
 #endif
+
   asm (
-       "1:      cmpldi %2, 0     \n\t"
-       "        beq- 1f          \n\t"
-       "        li 5, 16	 \n\t"
-       "        mtctr  %2	 \n\t"
+       "        cmpldi %2, 0     \n\t" // Check if n < 4 and
+       "        beq- 2f          \n\t" // if so bail out to 3.
+       "        li 5, 16         \n\t" // r5 = offset.
+       "        mtctr %2         \n\t" // Set counter.
        /********* Main Code ********/
-       "2:      lxvd2x  3, 0, %1 \n\t"
+       "1:      lxvd2x  3, 0, %1 \n\t" // Copy 4 elements.
        "        lxvd2x  4, %1, 5 \n\t"
        "        stxvd2x 3, 0, %0 \n\t"
        "        stxvd2x 4, %0, 5 \n\t"
-       "        addi %1, %1, 32  \n\t"
-       "      	addi %0, %0, 32  \n\t"
+       "        addi %1, %1, 32  \n\t" // Update src
+       "        addi %0, %0, 32  \n\t" // Update dst
        /****************************/
-       "	bdnz  2b	 \n\t"
-       "1:      nop              \n\t"
+       "        bdnz  1b         \n\t" // Dec. counter, if zero
+       "2:      nop              \n\t" // jump here (otherwise loop to 1).
         :
-        : "r"(dst), "r"(src), "r"(i)
-        : "memory", "3", "4", "5"
+        : "r"(dst), "r"(src), "r"(loop)
+        : "memory", "r3", "r4",
        );
 
+  // Deal with remainers and if n < 4.
   for (int j = i*4; j < n; ++j)
     dst[j] = src[j];
 }
@@ -108,11 +114,8 @@ int main(void)
 {
   printf("** Inline ASM for VSX test **\n\n");
 
-  uint64_t* source      = malloc(CHUNK_SIZE); // 16 MiB, or 1 MiB 64-bit element(s).
-  uint64_t* destination = malloc(CHUNK_SIZE); // 16 MiB, or 1 MiB 64-bit element(s).
-  uint64_t* zero	= malloc(CHUNK_SIZE); // To hold zeros.
-
-  bzero(zero, CHUNK_SIZE);
+  uint64_t* source      = malloc(BUFFER_SIZE); // 16 MiB, or 1 MiB 64-bit element(s).
+  uint64_t* destination = malloc(BUFFER_SIZE); // 16 MiB, or 1 MiB 64-bit element(s).
 
   int fd, rc;
 
@@ -124,8 +127,8 @@ int main(void)
     exit(1);
   }
 
-  rc = read(fd, (void*) source, CHUNK_SIZE);
-  if (rc != CHUNK_SIZE) {
+  rc = read(fd, (void*) source, BUFFER_SIZE);
+  if (rc != BUFFER_SIZE) {
     perror("read:");
     exit(1);
   } else
@@ -134,12 +137,26 @@ int main(void)
   printf("2. Exercising...\n");
 
   // Spend some time here.
-  for (int p = 0; p < 2500; ++p) {
+  for (int p = 0; p < 5000; ++p) {
     // Copy 16 MiB, or 1 M 64-bit (8 bytes) elements.
-    arraycopy(destination, source, 1024*1024);
+    arraycopy(destination, source, NUM_OF_ELEM_IN_BUFFER);
   }
 
   printf("2. Done.\n");
+
+  printf("3. Verifying if copy is ok...\n");
+
+
+  for (int p = 0; p < NUM_OF_ELEM_IN_BUFFER; ++p) {
+    if (destination[p] != source[p]) {
+      printf(">> Mismatch @%d: %#lx != %#lx\n", p,           \
+                                                destination, \
+                                                source);
+      exit(1);
+    }
+  }
+
+  printf("3. Done.\n");
 
   exit(0);
 }
