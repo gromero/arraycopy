bd1bb639a168cfc5ea0479ac5173e91d25ba6255
Gustavo Romero <gromero@br.ibm.com>
2016-03-16 19:35:13 -0300

    WIP

--- arraycopy.c
+++ arraycopy.c
@@ -10,7 +10,7 @@
 // Number of elements inside buffer and to be
 // loaded and stored between two different
 // locations in the storage.
-#define NUM_OF_ELEM_IN_BUFFER 1 <-----------
+#define NUM_OF_ELEM_IN_BUFFER 1024 * 1024 * 2 // 16 MiB
 
 // Number of bytes occupied by an element in buffer. Since
 // in this particular example we are using uint64_t, an element
@@ -23,7 +23,7 @@
 
 // Calculate buffer size in bytes able to hold a given
 // number of elements.
-#define BUFFER_SIZE NUM_OF_ELEM_IN_BUFFER * ELEMENT_SIZE
+#define BUFFER_SIZE NUM_OF_ELEM_IN_BUFFER*ELEMENT_SIZE
 
 
 #if !defined(VSX)
@@ -101,11 +101,11 @@ void arraycopy(uint64_t *dst, uint64_t *src, size_t n /* # of 64-bit elements */
        "2:      nop              \n\t" // jump here (otherwise loop to 1).
         :
         : "r"(dst), "r"(src), "r"(loop)
-        : "memory", "r3", "r4",
+        : "memory", "r3", "r4"
        );
 
   // Deal with remainers and if n < 4.
-  for (int j = i*4; j < n; ++j)
+  for (int j = loop * 4; j < n; ++j)
     dst[j] = src[j];
 }
 #endif
@@ -137,26 +137,27 @@ int main(void)
   printf("2. Exercising...\n");
 
   // Spend some time here.
-  for (int p = 0; p < 5000; ++p) {
-    // Copy 16 MiB, or 1 M 64-bit (8 bytes) elements.
+  for (int p = 0; p < 2500; ++p) {
+    // Copy 16 MiB
     arraycopy(destination, source, NUM_OF_ELEM_IN_BUFFER);
   }
 
   printf("2. Done.\n");
-
+/*
   printf("3. Verifying if copy is ok...\n");
 
 
   for (int p = 0; p < NUM_OF_ELEM_IN_BUFFER; ++p) {
     if (destination[p] != source[p]) {
-      printf(">> Mismatch @%d: %#lx != %#lx\n", p,           \
-                                                destination, \
-                                                source);
+      printf(">> Mismatch @%d: %#lx != %#lx\n", p,              \
+                                                destination[p], \
+                                                source[p]);
       exit(1);
     }
   }
 
   printf("3. Done.\n");
+*/
 
   exit(0);
 }
