2ae01aade8e36cab5f037f5f22f14421d0f6e3e3
Gustavo Romero <gromero@br.ibm.com>
2016-04-07 16:12:43 -0300

    Do not mess with binaries after the comparison

--- compare.sh
+++ compare.sh
@@ -3,6 +3,12 @@
 make clean
 make
 sudo perf record ./arraycopy
+cp arraycopy arraycopy_original
 mv arraycopy_vsx arraycopy
 sudo perf record ./arraycopy
+# Present the comparison.
 sudo perf diff
+# Restore original binary names.
+mv arraycopy arraycopy_vsx
+mv arraycopy_original arraycopy
+rm -fr perf.data*
