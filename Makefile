c2081af6e9e3233ebeb4e94c2957ced9a5a610e5
Gustavo Romero <gromero@br.ibm.com>
2016-04-15 17:18:53 -0300

    Include VMX and memcpy to build

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 all:
 	gcc -O0 -g arraycopy.c -o arraycopy
 	gcc -O0 -g arraycopy.c -o arraycopy_with_check -DCHECK
-	gcc -O0 -g arraycopy_vsx.c -o arraycopy_vsx -DVSX
-	gcc -O0 -g arraycopy_vsx.c -o arraycopy_vsx_with_check -DVSX -DCHECK
-
-debug:
-	gcc -O0 -g arraycopy.c -o arraycopy -DDEBUG
-	gcc -O0 -g arraycopy.c -o arraycopy_vsx -DVSX -DDEBUG
+	gcc -O0 -g arraycopy_vsx.c -o arraycopy_vsx
+	gcc -O0 -g arraycopy_vsx.c -o arraycopy_vsx_with_chech -DCHECK
+	gcc -O0 -g arraycopy_lvx.c -o arraycopy_lvx
+	gcc -O0 -g arraycopy_lvx.c -o arraycopy_lvx_with_check -DCHECK
+	gcc -O0 -g arraycopy_memcpy.c -o arraycopy_memcpy
+	gcc -O0 -g arraycopy_memcpy.c -o arraycopy_memcpy_with_check -DCHECK
 
 check:
 	gcc -O0 -g arraycopy.c -o arraycopy -DCHECK
@@ -15,4 +15,6 @@ check:
 clean:
 	rm -fr arraycopy arraycopy_vsx
 	rm -fr arraycopy_with_check arraycopy_vsx_with_check
+	rm -fr arraycopy_lvx  arraycopy_memcpy
+	rm -fr arraycopy_lvx_with_check arraycopy_memcpy_with_check
 	rm -fr perf*
--- arraycopy_memcpy.c
+++ arraycopy_memcpy.c
@@ -12,42 +12,6 @@
 #define NUM_ELEM_IN_BUFFER 1024*1024*2   	        // 2 M elements in buffer
 #define BUFFER_SIZE  	   NUM_ELEM_IN_BUFFER*ELEM_SIZE // 16 MiB in buffer
 
-void arraycopy(uint64_t *dst, uint64_t *src, size_t n)
-{
-  size_t i;
-
-  
-  // Number of 64-bit elements copied per loop. 
-  i = n / 4;
-
-  asm (
-       " 	li 3, 7+8	\n\t"
-       "	mtspr 3, 3	\n\t"
-       "        cmpldi %2, 0    \n\t"
-       "        beq   2f        \n\t"
-       "        mtctr %2	\n\t"
-       /********* Main Code ********/
-       "1:      ld  3, 0(%1)    \n\t"
-       "        ld  4, 8(%1)    \n\t"
-       "        ld  5,16(%1)    \n\t"
-       "        ld  6,24(%1)    \n\t"
-       "        std 3, 0(%0)    \n\t"
-       "        std 4, 8(%0)    \n\t"
-       "        std 5,16(%0)    \n\t"
-       "        std 6,24(%0)    \n\t"
-       /****************************/
-       "        addi %1, %1, 32 \n\t"
-       "        addi %0, %0, 32 \n\t"
-       "        bdnz+ 1b	\n\t"
-       "2:      nop             \n\t"
-        :
-        : "r"(dst), "r"(src), "r"(i)
-        : "memory", "r3", "r4", "r5", "r6"
-       );
-
-  for (int j = i*4; j < n; ++j)
-    dst[j] = src[j];
-}
 
 int main(void)
 {
