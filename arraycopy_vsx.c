4d27c2e77f18b4eef6c8af640aa4eb7e655cf8fa
Gustavo Romero <gromero@br.ibm.com>
2016-04-19 15:45:06 -0300

    Copy just 4 64-bit elements at time, just like in OpenJDK

--- arraycopy_vsx.c
+++ arraycopy_vsx.c
@@ -18,41 +18,25 @@ void arraycopy(uint64_t *dst, uint64_t *src, size_t n)
   size_t i = 0;
 
   // Copy 16 64-bit elements per loop.
-  i = n / 16;
+  i = n / 4;
 
   asm (
        "        cmpldi   %2,  0         \n\t"
        "        beq       2f            \n\t"
        "        li        6, 16	        \n\t"
-       "        li        7, 32	        \n\t"
-       "        li        8, 48 	\n\t"
        "        mtctr    %2	        \n\t"
        "        .align 4                \n\t"
        "1:      lxvd2x    6,  0, %1 	\n\t"
-       "        lxvd2x    7, %1,  6 	\n\t"
-       "  	lxvd2x    8, %1,  7 	\n\t"
-       "        lxvd2x    9, %1,  8	\n\t"
        "        stxvd2x   6,  0, %0 	\n\t"
-       "        addi     %1, %1, 64 	\n\t"
-       "        stxvd2x   7, %0,  6 	\n\t"
-       " 	stxvd2x   8, %0,  7	\n\t"
-       "    	stxvd2x   9, %0,  8	\n\t"
-       "        lxvd2x    6,  0, %1 	\n\t"
-       "        lxvd2x    7, %1,  6 	\n\t"
-       "      	addi     %0, %0, 64  	\n\t"
-       "        lxvd2x    8, %1,  7 	\n\t"
-       "        lxvd2x    9, %1,  8 	\n\t"
-       "        addi     %1, %1, 64 	\n\t"
-       "        stxvd2x   6,  0, %0 	\n\t"
-       "        stxvd2x   7, %0,  6 	\n\t"
-       " 	stxvd2x   8, %0,  7	\n\t"
-       "    	stxvd2x   9, %0,  8	\n\t"
-       "      	addi     %0, %0, 64  	\n\t"
+       "        lxvd2x    6,  6, %1 	\n\t"
+       "        stxvd2x   6,  6, %0 	\n\t"
+       "      	addi     %0, %0, 32  	\n\t"
+       "        addi     %1, %1, 32 	\n\t"
        "	bdnz+ 	 1b	        \n\t"
        "2:      nop                     \n\t"
         :
         : "r"(dst), "r"(src), "r"(i)
-        : "memory", "3", "4", "5", "6", "7", "8"
+        : "memory", "3", "4", "5", "6"
        );
 
   for (int j = i*16; j < n; ++j)
@@ -71,7 +55,6 @@ int main(void)
 
 #if defined(CHECK)
   printf("Verifying if copy is ok...\n");
-
 
   for (int p = 0; p < NUM_ELEM_IN_BUFFER; ++p) {
     if (destination[p] != source[p]) {
